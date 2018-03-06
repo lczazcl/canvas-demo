f54e19d6852616efc44d469acaf201f503a32aed
lichen <1193368261@qq.com>
2018-03-06 15:25:00 +0800

    添加画笔颜色、粗细，及下载功能

--- main.js
+++ main.js
@@ -2,11 +2,12 @@
 * @Author: Administrator
 * @Date:   2018-03-05 21:12:19
 * @Last Modified by:   Administrator
-* @Last Modified time: 2018-03-06 09:04:04
+* @Last Modified time: 2018-03-06 15:18:35
 */
 
 var yyy = document.getElementById('xxx')
 var context = yyy.getContext('2d')
+var lineWidth = 3
 
 //1.设置canvas尺寸，监听window变化
 autoSetCanvasSize(yyy)
@@ -20,13 +21,62 @@ listenToUser(yyy)
 
 //控制橡皮擦状态
 var eraserEnabled = false
+pen.onclick = function(){
+	eraserEnabled = false
+	pen.classList.add('active')
+	eraser.classList.remove('active')
+}
 eraser.onclick = function(){
 	eraserEnabled = true
-	actions.className = 'actions x'
+	eraser.classList.add('active')
+	pen.classList.remove('active')
 }
-brush.onclick = function(){
-	eraserEnabled = false
-	actions.className = 'actions'
+
+//设置画笔颜色
+black.onclick = function(){
+	context.fillStyle = 'black'
+	context.strokeStyle = 'black'
+	black.classList.add('active')
+	red.classList.remove('active')
+	blue.classList.remove('active')
+}
+red.onclick = function(){
+	context.fillStyle = 'red'
+	context.strokeStyle = 'red'
+	red.classList.add('active')
+	black.classList.remove('active')
+	blue.classList.remove('active')
+}
+blue.onclick = function(){
+	context.fillStyle = 'blue'
+	context.strokeStyle = 'blue'
+	blue.classList.add('active')
+	black.classList.remove('active')
+	red.classList.remove('active')
+}
+
+//清除画板
+clear.onclick = function(){
+	context.clearRect(0,0,yyy.width,yyy.height);
+}
+
+//设置画笔粗细
+thin.onclick = function(){
+	lineWidth = 5
+}
+thick.onclick = function(){
+	lineWidth = 10	
+}
+
+//保存画布
+download.onclick = function(){
+	var url = yyy.toDataURL("image/png")
+	var a = document.createElement('a')
+	document.body.appendChild(a)
+	a.href = url
+	a.download = '我的画布'
+	a.target = '_blank'
+	a.click()
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////
@@ -50,7 +100,7 @@ function autoSetCanvasSize(canvas){
 //画圆函数
 function drawCircle(x,y,radius){
     context.beginPath()
-    context.fillStyle = 'black'
+    //context.fillStyle = 'black'
     context.arc(x,y,radius,0,Math.PI*2) 
     context.fill()    //实心
 }
@@ -58,9 +108,9 @@ function drawCircle(x,y,radius){
 //画线函数
 function drawLine(x1,y1,x2,y2){
     context.beginPath()
-    context.strokeStyle = 'black'
+    //context.strokeStyle = 'black'
     context.moveTo(x1,y1)     //起点
-    context.lineWidth = 5
+    context.lineWidth = lineWidth
     context.lineTo(x2,y2)   //终点
     context.stroke()
     context.closePath()
